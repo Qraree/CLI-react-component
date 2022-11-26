a49c8c98106ba9feed1ae18711c6829e7e24ad92
Qrare <Metbezu@gmail.com>
2022-11-26 22:59:53 +0300

    added findit

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,35 @@
 #! /usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-
 const argv = require('minimist')(process.argv.slice(1));
+
+
+(async () => {
+    let search = new Promise((resolve, reject) => {
+        let finder = require('findit')('React component CLI');
+        finder.on('file', function (dir, stat) {
+            if (path.basename(dir) === 'bin') {
+                stop()  // found it
+                console.log('FOUND IT');
+                console.log(dir);
+                console.log(path.basename(dir));
+                resolve(dir)
+            }
+        });
+        finder.on('error', reject);
+    });
+    let found_file = await search;
+    // do some stuff with found_file
+})()
+
+
+
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
 const folderName = capitalizeFirstLetter(`${argv._[1]}`)
+
 fs.mkdirSync(folderName)
 fs.appendFileSync(path.resolve(folderName, folderName + '.jsx'),
 
